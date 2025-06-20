567e7e68703f3da09fb801481db574fff8ae06c2
ChernobylNTF <wallydd1983@gmail.com>
2025-06-20 17:54:23 -0300

    Update page.tsx

--- my-first-mini-app/src/app/(protected)/Home/page.tsx
+++ my-first-mini-app/src/app/(protected)/Home/page.tsx
@@ -6,7 +6,6 @@ import { UserInfo } from '@/components/UserInfo';
 import { Verify } from '@/components/Verify';
 import { ViewPermissions } from '@/components/ViewPermissions';
 import { Marble, TopBar } from '@worldcoin/mini-apps-ui-kit-react';
-import { Navigation } from '../components/Navigation';
 export default async function Home() {
   const session = await auth();
 
@@ -31,7 +30,6 @@ export default async function Home() {
         <Pay />
         <Transaction />
         <ViewPermissions />
-        <Navigation />
       </Page.Main>
     </>
   );
