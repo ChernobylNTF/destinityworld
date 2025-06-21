abff3c4ba610c5f1d59bc17ae7eb86ef96ca65be
ChernobylNTF <wallydd1983@gmail.com>
2025-06-20 21:35:39 -0300

    Update next.config.ts

--- my-first-mini-app/next.config.ts
+++ my-first-mini-app/next.config.ts
@@ -5,7 +5,9 @@ const nextConfig: NextConfig = {
     domains: ['static.usernames.app-backend.toolsforhumanity.com'],
   },
   allowedDevOrigins: [
-    'https://my-first-mini-app-chernobylntf-chernobyl-nft.vercel.app', // Tu URL de Vercel añadida aquí
+    'https://destinityworld-psi.vercel.app',
+    'https://destinityworld-chernobylntfs-projects.vercel.app',
+    'https://destinityworld-git-my-first-mini-app-chernobylntfs-projects.vercel.app',// Tu URL de Vercel añadida aquí
     // Si necesitas otros orígenes para desarrollo local, puedes añadirlos aquí también.
   ],
   reactStrictMode: false,
