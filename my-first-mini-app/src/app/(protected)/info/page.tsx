a8a989a174f2174b8fc075cdd8a6e57c94d80a17
ChernobylNTF <wallydd1983@gmail.com>
2025-06-21 22:40:35 -0300

    Update page.tsx

--- my-first-mini-app/src/app/(protected)/info/page.tsx
+++ my-first-mini-app/src/app/(protected)/info/page.tsx
@@ -40,6 +40,9 @@ const InfoPage = () => {
               <strong>Blockchain:</strong> Worldchain (Sepolia Testnet)
             </p>
             {/* Puedes añadir más información aquí */}
+            
+<a href="https://t.me/+Q6FBHtiGMdU1N2Zh" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Telegram</a>
+              
           </div>
         </div>
       </Page.Main>
