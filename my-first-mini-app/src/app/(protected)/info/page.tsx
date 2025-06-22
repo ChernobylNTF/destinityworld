e30d79ddebf5bc07cc5b37bd08142307c52d66c4
ChernobylNTF <wallydd1983@gmail.com>
2025-06-21 23:36:05 -0300

    Update page.tsx

--- my-first-mini-app/src/app/(protected)/info/page.tsx
+++ my-first-mini-app/src/app/(protected)/info/page.tsx
@@ -38,8 +38,9 @@ const InfoPage = () => {
               <p>
                 <strong>Redes Sociales</strong>
               </p>
+              <br />
             <a href="https://t.me/+Q6FBHtiGMdU1N2Zh" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Telegram</a>
-               <p><strong></strong></p>
+               <br />
             <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Twitter</a>
              </div> 
          </div>       
