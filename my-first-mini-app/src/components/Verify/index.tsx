3c97717ffd02eba1791b6ebb17e69c442a74e539
ChernobylNTF <wallydd1983@gmail.com>
2025-06-22 02:26:17 -0300

    Update index.tsx

--- my-first-mini-app/src/components/Verify/index.tsx
+++ my-first-mini-app/src/components/Verify/index.tsx
@@ -75,7 +75,11 @@ export const Verify = ({ onSuccess }: { onSuccess: () => void }) => {
         <Button
           onClick={() => onClickVerify(VerificationLevel.Device)}
           disabled={buttonState === 'pending'}
-          className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-2 py-1 text-center me-2 mb-2">Verificación
+          size="lg"
+          variant="tertiary"
+          className="w-full"
+          >
+          ⚠️ Verificación
            </Button>
       </LiveFeedback>
 
