e2939b9432e6c877eea22f3ab0b1e2d8753a51c2
ChernobylNTF <wallydd1983@gmail.com>
2025-06-21 23:19:01 -0300

    Update page.tsx

--- my-first-mini-app/src/app/(protected)/info/page.tsx
+++ my-first-mini-app/src/app/(protected)/info/page.tsx
@@ -28,15 +28,18 @@ const InfoPage = () => {
           <p className="text-lg font-semibold">Información de la Aplicación</p>
           <div className="p-4 border border-gray-700 rounded-lg">
             <p>Bienvenido a Destinity.</p>
-            <p>Sigue Nuestras Redes Sociales</p>
+            <p>Inicia sección todos los dias y acumula token DWD.</p>
             <br />
             <p>
-              <strong>Proyecto:</strong> Destinity Mini App
+              <strong>AIRDROP:</strong> Preparate para recibir nuestro Airdrop, completa tareas y sigue nuestras redes
             </p>
             {/* Puedes añadir más información aquí */}
+            <div className="grip w-full gap-4">
+              <p className="text-lg font-semibold">Redes Sociales</p>
             <a href="https://t.me/+Q6FBHtiGMdU1N2Zh" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Telegram</a>
-            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Twitter</a>
             
+            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Twitter</a>
+             </div> 
          </div>       
         </div>
       </Page.Main>
