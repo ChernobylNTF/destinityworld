03aed0f84fdf8621ba2adad6096c57cd791393a4
ChernobylNTF <wallydd1983@gmail.com>
2025-06-23 03:27:41 -0300

    Update route.ts

--- my-first-mini-app/src/app/api/verify-proof/route.ts
+++ my-first-mini-app/src/app/api/verify-proof/route.ts
@@ -20,6 +20,8 @@ export async function POST(req: NextRequest) {
   const { payload, action, signal } = (await req.json()) as IRequestPayload;
   const app_id = process.env.NEXT_PUBLIC_APP_ID as `app_${string}`;
 
+  console.log("Payload recibido del frontend:", payload);
+
   const verifyRes = (await verifyCloudProof(
     payload,
     app_id,
