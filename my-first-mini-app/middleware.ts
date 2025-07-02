45a2ccd3f430758700afa7fabe72ab8dfc5ad498
ChernobylNTF <wallydd1983@gmail.com>
2025-07-01 23:29:59 -0300

    Update middleware.ts

--- my-first-mini-app/middleware.ts
+++ my-first-mini-app/middleware.ts
@@ -1,1 +1,15 @@
-export { auth as middleware } from '@/auth';
+export { auth as middleware } from "@/auth"
+
+export const config = {
+  matcher: [
+    /*
+     * Coincide con todas las rutas de solicitud EXCEPTO las que comienzan con:
+     * - api (rutas de API)
+     * - _next/static (archivos estáticos de Next.js)
+     * - _next/image (archivos de optimización de imágenes de Next.js)
+     * - favicon.ico (el ícono de la pestaña)
+     * - / (la página de inicio pública)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|^/$).*)',
+  ],
+}
